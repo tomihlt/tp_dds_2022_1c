d1b0274fb658bfab8d7ddf840980e1f7eaba7f04
tomihlt <tomshaulet@gmail.com>
2022-10-03 10:19:20 -0300

    mejore un poco la interfaz de funciones

--- src/main/java/gui/Consultor/PanelFunciones.java
+++ src/main/java/gui/Consultor/PanelFunciones.java
@@ -28,23 +28,6 @@ import java.awt.event.ActionEvent;
 public class PanelFunciones extends JPanel
 {
 	private Main wWindow;
-	private JPanel panelBuscador;
-	private JLabel lblNewLabel;
-	private JLabel lblNewLabel_1;
-	private JLabel lblNewLabel_2;
-	private JTextField textField;
-	private JLabel lblNewLabel_3;
-	private JLabel lblNewLabel_4;
-	private JTextField textField_1;
-	private JLabel lblNewLabel_5;
-	private JLabel lblNewLabel_6;
-	private JTextField textField_2;
-	private JLabel lblNewLabel_7;
-	private JButton buscarButton;
-	private JLabel lblNewLabel_9;
-	private Component verticalStrut;
-	private Component verticalStrut_1;
-	private Component verticalStrut_2;
 	private JPanel panelDeDatos;
 	private JPanel panelDeBotones;
 	private JButton eliminarButton;
@@ -57,6 +40,27 @@ public class PanelFunciones extends JPanel
 	private Component horizontalStrut_2;
 	private Component horizontalStrut_3;
 	private Component verticalStrut_3;
+	private JPanel panelNorte;
+	private JPanel panelBuscador;
+	private JLabel lblNewLabel;
+	private Component verticalStrut;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
+	private JTextField textField;
+	private JLabel lblNewLabel_3;
+	private JLabel lblNewLabel_4;
+	private JTextField textField_1;
+	private JLabel lblNewLabel_5;
+	private JLabel lblNewLabel_6;
+	private JTextField textField_2;
+	private JLabel lblNewLabel_7;
+	private Component verticalStrut_1;
+	private JButton buscarButton;
+	private Component verticalStrut_2;
+	private JLabel lblNewLabel_8;
+	private Component horizontalStrut_4;
+	private Component horizontalStrut_5;
+	private Component verticalStrut_4;
 
 	/**
 	 * Create the panel.
@@ -65,152 +69,6 @@ public class PanelFunciones extends JPanel
 	{
 		this.wWindow = wWindow;
 		setLayout(new BorderLayout(0, 0));
-		
-		panelBuscador = new JPanel();
-		panelBuscador.setBorder(new LineBorder(Color.GRAY));
-		add(panelBuscador, BorderLayout.NORTH);
-		GridBagLayout gbl_panelBuscador = new GridBagLayout();
-		gbl_panelBuscador.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panelBuscador.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panelBuscador.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panelBuscador.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		panelBuscador.setLayout(gbl_panelBuscador);
-		
-		lblNewLabel = new JLabel("");
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.fill = GridBagConstraints.VERTICAL;
-		gbc_lblNewLabel.weighty = 0.5;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.gridx = 0;
-		gbc_lblNewLabel.gridy = 0;
-		panelBuscador.add(lblNewLabel, gbc_lblNewLabel);
-		
-		verticalStrut = Box.createVerticalStrut(25);
-		GridBagConstraints gbc_verticalStrut = new GridBagConstraints();
-		gbc_verticalStrut.insets = new Insets(0, 0, 5, 5);
-		gbc_verticalStrut.gridx = 2;
-		gbc_verticalStrut.gridy = 1;
-		panelBuscador.add(verticalStrut, gbc_verticalStrut);
-		
-		lblNewLabel_1 = new JLabel("");
-		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
-		gbc_lblNewLabel_1.fill = GridBagConstraints.VERTICAL;
-		gbc_lblNewLabel_1.weightx = 0.5;
-		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_1.gridx = 0;
-		gbc_lblNewLabel_1.gridy = 2;
-		panelBuscador.add(lblNewLabel_1, gbc_lblNewLabel_1);
-		
-		lblNewLabel_2 = new JLabel("Código");
-		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-		gbc_lblNewLabel_2.fill = GridBagConstraints.VERTICAL;
-		gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_2.gridx = 1;
-		gbc_lblNewLabel_2.gridy = 2;
-		panelBuscador.add(lblNewLabel_2, gbc_lblNewLabel_2);
-		
-		textField = new JTextField();
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(0, 0, 5, 5);
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridx = 2;
-		gbc_textField.gridy = 2;
-		panelBuscador.add(textField, gbc_textField);
-		textField.setColumns(10);
-		
-		lblNewLabel_3 = new JLabel("");
-		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
-		gbc_lblNewLabel_3.fill = GridBagConstraints.VERTICAL;
-		gbc_lblNewLabel_3.weightx = 0.1;
-		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_3.gridx = 3;
-		gbc_lblNewLabel_3.gridy = 2;
-		panelBuscador.add(lblNewLabel_3, gbc_lblNewLabel_3);
-		
-		lblNewLabel_4 = new JLabel("Nombre de la función");
-		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
-		gbc_lblNewLabel_4.fill = GridBagConstraints.VERTICAL;
-		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_4.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel_4.gridx = 4;
-		gbc_lblNewLabel_4.gridy = 2;
-		panelBuscador.add(lblNewLabel_4, gbc_lblNewLabel_4);
-		
-		textField_1 = new JTextField();
-		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_1.fill = GridBagConstraints.BOTH;
-		gbc_textField_1.gridx = 5;
-		gbc_textField_1.gridy = 2;
-		panelBuscador.add(textField_1, gbc_textField_1);
-		textField_1.setColumns(10);
-		
-		lblNewLabel_5 = new JLabel("");
-		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
-		gbc_lblNewLabel_5.fill = GridBagConstraints.VERTICAL;
-		gbc_lblNewLabel_5.weightx = 0.1;
-		gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_5.gridx = 6;
-		gbc_lblNewLabel_5.gridy = 2;
-		panelBuscador.add(lblNewLabel_5, gbc_lblNewLabel_5);
-		
-		lblNewLabel_6 = new JLabel("Nombre de la empresa");
-		GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
-		gbc_lblNewLabel_6.fill = GridBagConstraints.VERTICAL;
-		gbc_lblNewLabel_6.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_6.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel_6.gridx = 7;
-		gbc_lblNewLabel_6.gridy = 2;
-		panelBuscador.add(lblNewLabel_6, gbc_lblNewLabel_6);
-		
-		textField_2 = new JTextField();
-		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
-		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_2.fill = GridBagConstraints.BOTH;
-		gbc_textField_2.gridx = 8;
-		gbc_textField_2.gridy = 2;
-		panelBuscador.add(textField_2, gbc_textField_2);
-		textField_2.setColumns(10);
-		
-		lblNewLabel_7 = new JLabel("");
-		GridBagConstraints gbc_lblNewLabel_7 = new GridBagConstraints();
-		gbc_lblNewLabel_7.fill = GridBagConstraints.VERTICAL;
-		gbc_lblNewLabel_7.weightx = 0.5;
-		gbc_lblNewLabel_7.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_7.gridx = 9;
-		gbc_lblNewLabel_7.gridy = 2;
-		panelBuscador.add(lblNewLabel_7, gbc_lblNewLabel_7);
-		
-		verticalStrut_1 = Box.createVerticalStrut(20);
-		GridBagConstraints gbc_verticalStrut_1 = new GridBagConstraints();
-		gbc_verticalStrut_1.insets = new Insets(0, 0, 5, 5);
-		gbc_verticalStrut_1.gridx = 5;
-		gbc_verticalStrut_1.gridy = 3;
-		panelBuscador.add(verticalStrut_1, gbc_verticalStrut_1);
-		
-		buscarButton = new JButton("Buscar");
-		GridBagConstraints gbc_buscarButton = new GridBagConstraints();
-		gbc_buscarButton.fill = GridBagConstraints.VERTICAL;
-		gbc_buscarButton.insets = new Insets(0, 0, 5, 5);
-		gbc_buscarButton.gridx = 5;
-		gbc_buscarButton.gridy = 4;
-		panelBuscador.add(buscarButton, gbc_buscarButton);
-		
-		verticalStrut_2 = Box.createVerticalStrut(10);
-		GridBagConstraints gbc_verticalStrut_2 = new GridBagConstraints();
-		gbc_verticalStrut_2.insets = new Insets(0, 0, 5, 5);
-		gbc_verticalStrut_2.gridx = 5;
-		gbc_verticalStrut_2.gridy = 5;
-		panelBuscador.add(verticalStrut_2, gbc_verticalStrut_2);
-		
-		lblNewLabel_9 = new JLabel("");
-		GridBagConstraints gbc_lblNewLabel_9 = new GridBagConstraints();
-		gbc_lblNewLabel_9.fill = GridBagConstraints.VERTICAL;
-		gbc_lblNewLabel_9.insets = new Insets(0, 0, 0, 5);
-		gbc_lblNewLabel_9.gridx = 9;
-		gbc_lblNewLabel_9.gridy = 6;
-		panelBuscador.add(lblNewLabel_9, gbc_lblNewLabel_9);
 		
 		panelDeDatos = new JPanel();
 		add(panelDeDatos, BorderLayout.CENTER);
@@ -262,6 +120,165 @@ public class PanelFunciones extends JPanel
 			altaFuncion.setVisible(true);
 		});
 		panelDeBotones.add(nuevoButton);
+		
+		panelNorte = new JPanel();
+		add(panelNorte, BorderLayout.NORTH);
+		panelNorte.setLayout(new BorderLayout(0, 0));
+		
+		panelBuscador = new JPanel();
+		panelBuscador.setBorder(new LineBorder(Color.GRAY));
+		panelNorte.add(panelBuscador, BorderLayout.CENTER);
+		GridBagLayout gbl_panelBuscador = new GridBagLayout();
+		gbl_panelBuscador.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panelBuscador.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panelBuscador.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panelBuscador.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panelBuscador.setLayout(gbl_panelBuscador);
+		
+		lblNewLabel = new JLabel("");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.weighty = 0.5;
+		gbc_lblNewLabel.fill = GridBagConstraints.VERTICAL;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		panelBuscador.add(lblNewLabel, gbc_lblNewLabel);
+		
+		verticalStrut = Box.createVerticalStrut(25);
+		GridBagConstraints gbc_verticalStrut = new GridBagConstraints();
+		gbc_verticalStrut.insets = new Insets(0, 0, 5, 5);
+		gbc_verticalStrut.gridx = 2;
+		gbc_verticalStrut.gridy = 1;
+		panelBuscador.add(verticalStrut, gbc_verticalStrut);
+		
+		lblNewLabel_1 = new JLabel("");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.weightx = 0.5;
+		gbc_lblNewLabel_1.fill = GridBagConstraints.VERTICAL;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 2;
+		panelBuscador.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		lblNewLabel_2 = new JLabel("Código");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.fill = GridBagConstraints.VERTICAL;
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 1;
+		gbc_lblNewLabel_2.gridy = 2;
+		panelBuscador.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.gridx = 2;
+		gbc_textField.gridy = 2;
+		panelBuscador.add(textField, gbc_textField);
+		
+		lblNewLabel_3 = new JLabel("");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.weightx = 0.1;
+		gbc_lblNewLabel_3.fill = GridBagConstraints.VERTICAL;
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 3;
+		gbc_lblNewLabel_3.gridy = 2;
+		panelBuscador.add(lblNewLabel_3, gbc_lblNewLabel_3);
+		
+		lblNewLabel_4 = new JLabel("Nombre de la función");
+		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+		gbc_lblNewLabel_4.fill = GridBagConstraints.VERTICAL;
+		gbc_lblNewLabel_4.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_4.gridx = 4;
+		gbc_lblNewLabel_4.gridy = 2;
+		panelBuscador.add(lblNewLabel_4, gbc_lblNewLabel_4);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.fill = GridBagConstraints.BOTH;
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.gridx = 5;
+		gbc_textField_1.gridy = 2;
+		panelBuscador.add(textField_1, gbc_textField_1);
+		
+		lblNewLabel_5 = new JLabel("");
+		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
+		gbc_lblNewLabel_5.weightx = 0.1;
+		gbc_lblNewLabel_5.fill = GridBagConstraints.VERTICAL;
+		gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_5.gridx = 6;
+		gbc_lblNewLabel_5.gridy = 2;
+		panelBuscador.add(lblNewLabel_5, gbc_lblNewLabel_5);
+		
+		lblNewLabel_6 = new JLabel("Nombre de la empresa");
+		GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
+		gbc_lblNewLabel_6.fill = GridBagConstraints.VERTICAL;
+		gbc_lblNewLabel_6.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_6.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_6.gridx = 7;
+		gbc_lblNewLabel_6.gridy = 2;
+		panelBuscador.add(lblNewLabel_6, gbc_lblNewLabel_6);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.fill = GridBagConstraints.BOTH;
+		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_2.gridx = 8;
+		gbc_textField_2.gridy = 2;
+		panelBuscador.add(textField_2, gbc_textField_2);
+		
+		lblNewLabel_7 = new JLabel("");
+		GridBagConstraints gbc_lblNewLabel_7 = new GridBagConstraints();
+		gbc_lblNewLabel_7.weightx = 0.5;
+		gbc_lblNewLabel_7.fill = GridBagConstraints.VERTICAL;
+		gbc_lblNewLabel_7.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_7.gridx = 9;
+		gbc_lblNewLabel_7.gridy = 2;
+		panelBuscador.add(lblNewLabel_7, gbc_lblNewLabel_7);
+		
+		verticalStrut_1 = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut_1 = new GridBagConstraints();
+		gbc_verticalStrut_1.insets = new Insets(0, 0, 5, 5);
+		gbc_verticalStrut_1.gridx = 5;
+		gbc_verticalStrut_1.gridy = 3;
+		panelBuscador.add(verticalStrut_1, gbc_verticalStrut_1);
+		
+		buscarButton = new JButton("Buscar");
+		GridBagConstraints gbc_buscarButton = new GridBagConstraints();
+		gbc_buscarButton.fill = GridBagConstraints.VERTICAL;
+		gbc_buscarButton.insets = new Insets(0, 0, 5, 5);
+		gbc_buscarButton.gridx = 5;
+		gbc_buscarButton.gridy = 4;
+		panelBuscador.add(buscarButton, gbc_buscarButton);
+		
+		verticalStrut_2 = Box.createVerticalStrut(10);
+		GridBagConstraints gbc_verticalStrut_2 = new GridBagConstraints();
+		gbc_verticalStrut_2.insets = new Insets(0, 0, 5, 5);
+		gbc_verticalStrut_2.gridx = 5;
+		gbc_verticalStrut_2.gridy = 5;
+		panelBuscador.add(verticalStrut_2, gbc_verticalStrut_2);
+		
+		lblNewLabel_8 = new JLabel("");
+		GridBagConstraints gbc_lblNewLabel_8 = new GridBagConstraints();
+		gbc_lblNewLabel_8.fill = GridBagConstraints.VERTICAL;
+		gbc_lblNewLabel_8.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_8.gridx = 9;
+		gbc_lblNewLabel_8.gridy = 6;
+		panelBuscador.add(lblNewLabel_8, gbc_lblNewLabel_8);
+		
+		horizontalStrut_4 = Box.createHorizontalStrut(20);
+		panelNorte.add(horizontalStrut_4, BorderLayout.WEST);
+		
+		horizontalStrut_5 = Box.createHorizontalStrut(20);
+		panelNorte.add(horizontalStrut_5, BorderLayout.EAST);
+		
+		verticalStrut_4 = Box.createVerticalStrut(20);
+		panelNorte.add(verticalStrut_4, BorderLayout.NORTH);
 
 	}
 
